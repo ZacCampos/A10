2f3491cc65f3847f4981c21831d5f991f9c497d6
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-24 13:46:17 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+
+#include "header.h"
+
+int main()
+{
+	Heading();
+
+	const int ARRAY_SIZE = 10;
+
+	string namesAr[ARRAY_SIZE];
+	int idAr[ARRAY_SIZE];
+	float balanceAr[ARRAY_SIZE];
+	string fileName;
+
+	fileName = "InFile.txt";
+
+	Input(ARRAY_SIZE, namesAr, idAr, balanceAr, fileName);
+
+
+
+
+
+
+
+
+
+	return 0;
+}
